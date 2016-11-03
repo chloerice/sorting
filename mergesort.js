765b91ca703a7f360693341556abf5088f1cf904
Chloe Rice <chloe.mi.rice@gmail.com>
2016-11-03 13:07:22 -0400

    switch

--- mergesort.js
+++ mergesort.js
@@ -3,5 +3,16 @@ function split(wholeArray) {
 	var firstHalf = wholeArray.slice(0,wholeArray.length/2);
 	var secondHalf = wholeArray.slice(wholeArray.length/2);
 
-  return [firstHalf, secondHalf];
+	if (firstHalf.length === 1 && secondHalf.length === 1) {
+		return [firstHalf, secondHalf];
+	}
+
+	if (firstHalf.length > 1) {
+		firstHalf = firstHalf.concat(split(firstHalf));
+	}
+
+	if (secondHalf.length > 1) {
+		secondHalf = secondHalf.concat(split(secondHalf));
+	}
+	// console.log("firstHalf" ${firstHalf}, "secondHalf" ${secondHalf});
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -4,8 +4,6 @@ describe('Split Array function', function() {
   });
 
   it('is able to split arrays until each array has a length of 1', function() {
-    expect( split([1, 2, 3, 4])).toEqual()
+    expect( split([3, 6, 2, 7, 9, 1, 8, 5])).toEqual([[3], [6], [2], [7], [9], [1], [8], [5]])
   });
-
-
 });
