d881f22f4cfb23856adf8c76b85c53b96c1e1f7d
Chloe Rice <chloe.mi.rice@gmail.com>
2016-11-03 15:30:43 -0400

    last commit

--- mergesort.js
+++ mergesort.js
@@ -3,27 +3,44 @@ function split(wholeArray) {
 	var firstHalf = wholeArray.slice(0,wholeArray.length/2);
 	var secondHalf = wholeArray.slice(wholeArray.length/2);
 
-	// console.log(firstHalf, secondHalf);
-
-	// if (firstHalf.length === 1 && secondHalf.length === 1) {
-	// 	return [firstHalf, secondHalf];
-	// }
-
-	// if (firstHalf.length > 1) {
-	// 	firstHalf = split(firstHalf);
-	// }
-
-	// if (secondHalf.length > 1) {
-	// 	secondHalf = split(secondHalf);
-	// }
-	
 	return [firstHalf, secondHalf];
 }
 
 function merge(arr1, arr2){
 	var mergedArr = [];
 
-	for(var i=0; )
+	for(var i = 0; i < arr1.length; i++ ) {
+		if (arr1[i] > arr2[i]) {
+			mergedArr.push(arr2[i]);
+			mergedArr.push(arr1[i]);
+		} else {
+			mergedArr.push(arr1[i]);
+			mergedArr.push(arr2[i]);
+		}
+	}
 
 	return mergedArr;
+}
+
+function mergeSort (arr) {
+	var splitArr = split(arr);
+	var firstHalf = splitArr[0];
+	var secondHalf = splitArr[1];
+	var sortedArr = [];
+
+	//BASE CASE:
+	if (firstHalf.length === 1 && secondHalf.length === 1) {
+	      sortedArr = sortedArr.concat(merge(firstHalf, secondHalf));
+	 }
+
+	//  if (firstHalf.length > 1) {
+	//      firstHalf = split(firstHalf);
+	//  }
+	 //
+	//  if (secondHalf.length > 1) {
+	//      secondHalf = split(secondHalf);
+	//  }
+
+
+	return sortedArr;
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -12,8 +12,14 @@ describe('Split Array function', function() {
   });
 });
 
-describe('Merge Sort', function(){
+describe('Merge', function(){
   it('is able to merge two sorted arrays into one sorted array', function(){
-    expect().toEqual()
+    expect(merge([1, 4, 6, 8], [3, 5, 7, 9])).toEqual([1, 3, 4, 5, 6, 7, 8, 9])
+  });
+});
+
+describe('Merge Sort', function(){
+  it('should sort an unsorted array and return a sorted array', function(){
+    expect(mergeSort([3, 6, 2, 7, 9, 1, 8, 5, 4])).toEqual([1, 3, 4, 5, 6, 7, 8, 9])
   });
 });
